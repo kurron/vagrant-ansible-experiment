8d23725e1c6932ea83dc542e324e222e7084e0af
Ron Kurr <rkurr@jvmguy.com>
2014-11-01 11:23:40 -0400

    Got Ansible to work under Windows

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ansible-playbook --verbose --connection=local --limit=${HOSTNAME} --inventory-file=/vagrant/ansible/inventory.ini /vagrant/ansible/playbook.yml
+# we have to do this in case we are running under Windows
+cp -r /vagrant/ansible /tmp
+chmod -x /tmp/ansible/inventory.ini
+
+ansible-playbook --verbose --connection=local --limit=${HOSTNAME} --inventory-file=/tmp/ansible/inventory.ini /tmp/ansible/playbook.yml
